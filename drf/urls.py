d0a9b321cb5eb3006d608d4e9f21eb3e154bbb1f
Sergei Strygin <sew1982@gmail.com>
2024-08-13 11:18:56 +0300

    change urls ...details

--- drf/urls.py
+++ drf/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/womenlist/', WomenAPIList.as_view()),
     path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
