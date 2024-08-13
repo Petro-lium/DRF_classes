ea4929efca41bea8140b742e66e21d73065bc156
Sergei Strygin <sew1982@gmail.com>
2024-08-13 14:20:06 +0300

    add @action

--- women/views.py
+++ women/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
-from .models import Women
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Women, Category
 from .serializers import WomenSerializer
 
 
 class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+    
+    @action(methods=['get'], detail=True) # False возвращается список категорий, если True то одна запись
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 
 '''
 class WomenAPIList(generics.ListCreateAPIView):
