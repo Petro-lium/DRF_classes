716d8046f9302198fdada1bff7074d81f4820796
Sergei Strygin <sew1982@gmail.com>
2024-08-14 18:47:44 +0300

    import permissions

--- women/views.py
+++ women/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
 from .models import Women, Category
 from .serializers import WomenSerializer
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
 
 class WomenViewSet(viewsets.ModelViewSet):
