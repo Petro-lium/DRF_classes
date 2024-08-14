bc7cc1980451fc1be95ddad3aa6667da8435ed7e
Sergei Strygin <sew1982@gmail.com>
2024-08-14 15:18:12 +0300

    djoser and add seconduser

--- drf/settings.py
+++ drf/settings.py
@@ -132,7 +132,14 @@ REST_FRAMEWORK = {
             'rest_framework.renderers.JSONRenderer',
             'rest_framework.renderers.BrowsableAPIRenderer',
         ],
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
--- drf/urls.py
+++ drf/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from women.views import *
 from rest_framework import routers
 
@@ -26,6 +26,8 @@ router.register(r'women', WomenViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('api/v1/', include(router.urls)),  # http://127.0.0.1:8000/api/v1/women/
     # path('api/v1/womenlist/', WomenAPIList.as_view()),
     # path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
